c7f8c04934459f1dc67462d924ac103c290de055
David Cornu <david@usepatch.com>
2023-09-19 14:27:55 -0400

    Remove extra import

--- app.js
+++ app.js
@@ -2,7 +2,3 @@ import "@shoelace-style/shoelace/dist/themes/light.css";
 
 import "@shoelace-style/shoelace/dist/components/option/option.js";
 import "@shoelace-style/shoelace/dist/components/select/select.js";
-
-
-import "@shoelace-style/shoelace/dist/components/radio/radio.js";
-import "@shoelace-style/shoelace/dist/components/radio-group/radio-group.js";
